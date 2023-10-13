72df00777bd041231bf632ffb7c4024c0bf78819
muhammed shibili <muhmdshanoob@gmail.com>
2023-10-13 16:00:01 +0530

    Initial commit-5

--- script/script.js
+++ script/script.js
@@ -19,46 +19,20 @@ navLinks.forEach((l) => {
 })
 
 
-//email validation
+$("#contact-form").submit((e)=>{
+    e.preventDefault()
+    $.ajax({
+        url:"https://script.google.com/macros/s/AKfycbxrIA9dcT3nAz_Hxmqq77ep1r-bVnK1GZ7fOirFIefQTfq90Kd8AUc_SflrcOM6hOeY/exec",
+        data:$("#contact-form").serialize(),
+        method:"post",
+        success:function (response){
+            alert("Form submitted successfully")
+            window.location.reload()
+            //window.location.href="https://google.com"
+        },
+        error:function (err){
+            alert("Something Error")
 
-document.getElementById("contact-form").addEventListener("submit", function (event) {
-    event.preventDefault();
-
-    // Validation functions
-    function validateName(name) {
-        return name.length > 0;
-    }
-
-    function validateEmail(email) {
-        // A simple email validation regex
-        const emailPattern = /^[a-zA-Z0-9._%+-]+@[a-zA-Z0-9.-]+\.[a-zA-Z]{2,}$/;
-        return emailPattern.test(email);
-    }
-
-    // Form fields
-    const name = document.querySelector('input[name="name"]').value;
-    const email = document.querySelector('input[name="email"]').value;
-
-    // Error messages
-    const nameError = document.getElementById('name-error');
-    const emailError = document.getElementById('email-error');
-
-    // Validating the fields
-    if (!validateName(name)) {
-        nameError.innerText = "Please enter your name.";
-    } else {
-        nameError.innerText = "";
-    }
-
-    if (!validateEmail(email)) {
-        emailError.innerText = "Please enter a valid email address.";
-    } else {
-        emailError.innerText = "";
-    }
-    // Submit the form if all validations pass
-    if (validateName(name) && validateEmail(email)) {
-    
-        this.submit(); // This submits the form 
-    }
-
-});
+        }
+    })
+})
